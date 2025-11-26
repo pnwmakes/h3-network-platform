3d1dffedb76b2123585bbf1e4eb9627db63ddb75
James Melzark <pnwmakes@gmail.com>
2025-11-26 09:56:52 -0800

    Add trustHost and useSecureCookies for Netlify deployment

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -38,17 +38,8 @@ export const authOptions: NextAuthOptions = {
         strategy: 'jwt',
         maxAge: 30 * 24 * 60 * 60, // 30 days
     },
-    cookies: {
-        sessionToken: {
-            name: `${process.env.NODE_ENV === 'production' ? '__Secure-' : ''}next-auth.session-token`,
-            options: {
-                httpOnly: true,
-                sameSite: 'lax',
-                path: '/',
-                secure: process.env.NODE_ENV === 'production',
-            },
-        },
-    },
+    useSecureCookies: process.env.NODE_ENV === 'production',
+    trustHost: true, // Required for Netlify
     debug: process.env.NODE_ENV === 'development',
     providers: [
         GoogleProvider({
